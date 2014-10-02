f32ad7308ac11e1b5fb282e00a2fa56f43a3d75a
Argeiphontes <christopher.ruggles@aya.yale.edu>
2014-10-02 14:00:52 -0500

    Initial Commit

---
+++ CodeChallenge1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CodeChallenge1
+//
+//  Created by Christopher on 10/2/14.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
